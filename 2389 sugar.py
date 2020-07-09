4d68751cba8fe3cb045a1c1cdebce34c789a7c62
EUNJEONGMUN <munej26@gmail.com>
2020-07-09 21:32:34 +0900

    sugar_try1

--- 2389 sugar.py
+++ 2389 sugar.py
@@ -4,25 +4,41 @@
 def sugar(n):
     count_3 = 0
     count_5 = 0
+    mark = 0
     while n>2:
         if n%8==0:
-            count_3 = count_3 + n//8
-            count_5 = count_5 + n//8
-            n = n%8
+            count_3 = count_5 = n//8
+            n = 0
             break
         
         elif n%5==0:
-            count_5 = count_5 + n//5
-            n = n%5
+            count_5 = n//5
+            n = 0
+            break
+        
+        elif n%3 == 0 and (n//3)<5:
+            count_3 = n//3
+            n = 0
             break
 
-        n = n%5
-        count_5 = n//5
+        else:
+            if mark == 0:
+                count_5 = count_5 + n//5
+                n = n%5
+            
 
-        if n
         if n%3==0:
             count_3 = count_3 + n//3
-            n = n%3
+            n = 0
+            break
+
+        elif (n==1) and (mark == 0):
+            n += 5
+            count_5 -= 1
+            mark = 1
+
+        else:
+            break
 
 
 
@@ -35,4 +51,6 @@ def sugar(n):
 
 n = int(input())
 print(sugar(n))
+
+    
 
