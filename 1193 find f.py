432845365d9b819d1d46a301bce392ff02cfbff1
EUNJEONGMUN <munej26@gmail.com>
2020-07-10 15:48:01 +0900

    find_f ok but time error

--- 1193 find f.py
+++ 1193 find f.py
@@ -1,2 +1,30 @@
 #20200709 1193 분수찾기
 
+def find_f(n):
+    first = 1
+    second = 1
+    count = 1
+
+    while count!=n:
+        if (first==1) and (second%2==1):
+            second += 1
+            mark = 0  #0 -> down
+        elif (second == 1) and (first%2==0):
+            first += 1
+            mark = 1  #1 -> up
+
+        elif mark==0:
+            first += 1
+            second -= 1
+        else:
+            first -= 1
+            second += 1
+        count += 1
+
+    print(first,second,sep='/')
+
+
+
+
+n = int(input())
+find_f(n)
