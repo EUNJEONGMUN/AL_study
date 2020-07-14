b0446394a1351b99b7f29a34b4c46a78cd837262
EUNJEONGMUN <munej26@gmail.com>
2020-07-14 21:51:08 +0900

    mom ok

--- 2775 mom.py
+++ 2775 mom.py
@@ -1,5 +1,5 @@
 #2775 부녀회장이 될테야
-
+'''
 
 def mom(k,n):
     if n == 1:
@@ -11,7 +11,19 @@ def mom(k,n):
         mark = mark + mark+1
     return num
 
+'''
 
-k = int(input())
-n = int(input())
-print(mom(k,n))
+def mom(k,n):
+    L = [i for i in range(15)]  #[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+    
+    for cmd in range(k):
+        for gh in range(1,n+1):
+            L[gh] = L[gh-1]+L[gh]
+
+    print(L[n])
+
+count = int(input())
+for i in range(count):
+    k = int(input())
+    n = int(input())
+    mom(k,n)
