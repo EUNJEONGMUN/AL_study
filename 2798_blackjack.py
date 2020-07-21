ad9d5f02cd7b4a092139fc1cee846692e0ccf08f
EUNJEONGMUN <munej26@gmail.com>
2020-07-21 16:01:36 +0900

    blackjack try4

--- 2798_blackjack.py
+++ 2798_blackjack.py
@@ -1,4 +1,4 @@
-#2798 블랙잭
+    #2798 블랙잭
 #20200716
 '''
 def blackjack(n,A,M):
@@ -22,6 +22,7 @@ def blackjack(n,A,M):
 
     return A[p]+A[q]+A[r]
 '''
+'''
 def blackjack2(n,A,M):
     A.sort()
     p,q,r = 0,1,2
@@ -42,14 +43,50 @@ def blackjack2(n,A,M):
     return A[p]+A[q]+A[r]
 
 
+'''
 
 
+def blackjack3(n,A,M):
+    A.sort()
+    p,q,r = 0,1,2
+    sub = M-(A[0]+A[1]+A[2])
+    sum_pqr = A[0]+A[1]+A[2]
 
+    while True:
+        if ((A[p]+A[q]+A[r])>=M) and (p==n-3):
+            return sum_pqr
+        if r!=n-1:
+            r+=1
+            if sub>M-(A[p]+A[q]+A[r])>=0:
+                sub = M-(A[p]+A[q]+A[r])
+                sum_pqr = M-sub
+        elif q!=n-2:
+            q+=1
+            if sub>M-(A[p]+A[q]+A[r])>=0:
+                sub = M-(A[p]+A[q]+A[r])
+                sum_pqr = M-sub
+        else:
+            p+=1
+            if sub>M-(A[p]+A[q]+A[r]>=0):
+                sub = M-(A[p]+A[q]+A[r])
+                sum_pqr = M-sub
+    
+
+
+def blackjack4(n,A,M):
+    A.sort()
+    sum_pqr = A[0]+A[1]+A[2]
+    while True:
+        if r!=n-1:
+            r+=1
+            
+            
+        
 
 n,M = input().split()
 n,M = int(n), int(M)
 A = list(map(int, input().split()))
-print(blackjack(n,A,M))
+print(blackjack3(n,A,M))
         
         
 
