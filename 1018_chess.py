6e5ce2b183a53c8207973b0e87d50ccc850ece2e
EUNJEONGMUN <munej26@gmail.com>
2020-07-23 16:18:53 +0900

    chess start

---
+++ 1018_chess.py
@@ -0,0 +1,32 @@
+#1018 체스판 다시 칠하기
+#20200723
+
+def chess(N,M,L):
+    different = 0
+    want1 = [[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW]]
+    want2 = [[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB]]
+
+    for a in range(8):   # 시작점 L[a][b]
+        for b in range(M-7):
+            for c in range(8):
+                
+            
+        
+    
+
+
+
+
+
+
+N,M = input().split()
+N,M = int(N),int(M)
+L = []
+
+for i in range(N):
+    L.append(input())
+
+chess(N,M,L)
+
+
+    
