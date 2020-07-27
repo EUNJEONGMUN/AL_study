da495329ea66ecd99bcd70dd02a81fa37d674d59
EUNJEONGMUN <munej26@gmail.com>
2020-07-27 20:56:38 +0900

    chess try1

--- 1018_chess.py
+++ 1018_chess.py
@@ -5,17 +5,17 @@ def chess(N,M,L):
     different = 0
     want1 = [[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW]]
     want2 = [[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB],[BWBWBWBW],[WBWBWBWB]]
+    count_list = []
+
 
     for a in range(8):   # 시작점 L[a][b]
         for b in range(M-7):
             for c in range(8):
+                for d in range(8):    # c -> 01234567
+
+                    if want1[a][b][d] == L
+
                 
-            
-        
-    
-
-
-
 
 
 
