5cc5f77a3be38761dd2faeb977bb7fd4c4177331
EUNJEONGMUN <munej26@gmail.com>
2020-07-29 15:44:40 +0900

    star try1

--- 2447_star10.py
+++ 2447_star10.py
@@ -4,26 +4,31 @@
 def star_10(n):
 
     if n == 3:
+        print('***')
+        print('* *')
+        print('***')
+        '''
         for i in range(3):
             if i == 1:
-                print('* *')
+                print('*'*(n//3)+' '*(n//3)+'*'*(n//3))
             else:
-                print('***')
+                print('*'*n)
+        
 
+        print('***'*(n//3))
+        print('* *'*(n//3))
+        print('***'*(n//3))
+        '''
+
+        
     else:
-        for i in range(3):
-            if i == 1:
-                star_10(n//3)
-                print()
-                star_10(n//3)
-                
-            else:
-                star_10(n//3)
-                star_10(n//3)
-                star_10(n//3)
+        star_10(n//3)
+        star_10(n//3)
+        star_10(n//3)
+        star_10(n//3)
+        star_10(n//3)
+        
 
-            
-                
             
 
 
