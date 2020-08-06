dbb6ba2171af5ecc430d6734e3e1a9f1a71f8344
EUNJEONGMUN <munej26@gmail.com>
2020-08-06 13:39:29 +0900

    math_insert_int ok but time error

--- 14888_math_insert_int.py
+++ 14888_math_insert_int.py
@@ -1,8 +1,6 @@
 #14888 연산자 끼워넣기
 #20200730
 
-
-
 from itertools import permutations
 
 
@@ -20,36 +18,28 @@ def math_insert(n,num,op):
     for _ in range(op[3]):
         OP.append('/')
 
-    my_op = permutations(OP,len(OP)) # 연산자 수열
-
-    MMYY_OOPP = []
-    
-
-    for i in my_op:
-        MMYY_OOPP.append(''.join(map(str,i)))
+    my_op = list(permutations(OP,len(OP))) # my_op 연산자 수열
 
 
-    for a in range(len(MMYY_OOPP)): #a -> 0 1 2 3 4.....60
-        answer = 0
-        my_answer = num[0]  #첫번째숫자 문자열
-        for b in range(n-1):  # b -> 0 1 2 3 4
-            if MMYY_OOPP[a][b] == '/' and int(num[b+1])<0:
-                temp = int(num[b+1])
-                my_answer += '//'+str(-temp)
+    for a in range(len(my_op)): #a -> 0 1 2 3 4.....60
+       
+        my_answer = num[0]  #첫번째숫자 int
+        
+        for b in range(n-1):  # b -> 0 1 2 3 4 5
+            if my_op[a][b] == '+':
+                my_answer += num[b+1]
+            elif my_op[a][b] == '-':
+                my_answer -= num[b+1]
+            elif my_op[a][b] == '*':
+                my_answer *= num[b+1]
+            elif my_op[a][b] == '/' and my_answer<0:
+                temp = -num[b+1]
+                my_answer = my_answer // temp
+                my_answer = -my_answer
+            elif my_op[a][b] == '/' and my_answer>0:
+                my_answer = my_answer // num[b+1]
 
-                answer = eval(my_answer)
-                my_answer = str(-answer)
-                continue
-            elif MMYY_OOPP[a][b] == '/' and int(num[b+1])>0:
-                my_answer += '//'+num[b+1]
-                
-            else:
-                my_answer += MMYY_OOPP[a][b]+num[b+1]
-
-            answer = eval(my_answer)
-            my_answer = str(answer)
-
-        my_max.append(int(my_answer))
+        my_max.append(my_answer)
         
 
     print(max(my_max), min(my_max))
@@ -58,7 +48,7 @@ def math_insert(n,num,op):
 
 
 n = int(input())
-num = list(map(str,input().split()))# 숫자의 순서는 바뀌지 않음.
+num = list(map(int,input().split()))# 숫자의 순서는 바뀌지 않음.
 op = list(map(int,input().split())) #    + - *  // 의 개수
 
 math_insert(n,num,op)  #연산자 개수는 숫자 개수보다 하나 적음.
