dd530df5fa873358311243d46db042d05b19e895
EUNJEONGMUN <munej26@gmail.com>
2020-08-19 14:54:53 +0900

    maze_search start

---
+++ 2178_maze_search.py
@@ -0,0 +1,33 @@
+#2178 미로탐색
+#20200813
+
+def solution(L):
+
+    answer = [[None]*m]*n
+
+
+    
+    for a in range(n):
+        for b in range(m):
+            if L[a][b] == 1:
+                if L[a-1]
+                
+            
+
+
+
+
+
+
+
+
+
+
+n,m = input().split()
+n,m = int(n),int(m)
+L = []
+
+for _ in range(n):
+    L.append(list(map(int,input().split())))
+
+solution(L)
