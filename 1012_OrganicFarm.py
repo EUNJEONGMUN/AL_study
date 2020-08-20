72ced610b20842a7ab0e96ea6f2459d865c8cfe4
EUNJEONGMUN <munej26@gmail.com>
2020-08-20 14:39:39 +0900

    OrganicFarm ok but not my

--- 1012_OrganicFarm.py
+++ 1012_OrganicFarm.py
@@ -3,7 +3,6 @@
 
 def bfs(i,j):
     Q = [(i,j)]
-    Farm[i][j] = 0
 
     while Q:
         a,b = Q[0][0], Q[0][1]
@@ -12,48 +11,38 @@ def bfs(i,j):
             x = a + dx[k]
             y = b + dy[k]
 
-            if 0<=x<n and 0<=y<n and Farm[x][y] == 1:
+            if 0<=x<N and 0<=y<M and Farm[x][y] == 1:
                 Farm[x][y] = 0
                 Q.append((x,y))
-
-
-def solution(M,N,Farm):   #가로 M // 세로 N
-    count = 0
-    for a in range(N):
-        for b in range(M):
-            if Farm[a][b] == 1:
-                count += 1
-                bfs(a,b)
-
-    print(count)
-    
 
 
 n = int(input())
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-'''
 for _ in range(n):
     M,N,K = input().split()
     M,N,K = int(M),int(N),int(K)
 
-    Farm = [[0]*M]*N
+    Farm = [[0]*M for _ in range(N)]
+    count = 0
 
     for _ in range(K):
-        x,y = input().split()
-        x,y = int(x),int(y)
-        Farm[y][x] = 1
+        a,b = map(int,input().split())
+        Farm[b][a] = 1
 
-    solution(M,N,Farm)
+    for x in range(N):
+        for y in range(M):
+            if Farm[x][y] == 1:
+                bfs(x,y)
+                Farm[x][y] = 0
+                count += 1
 
-'''
-M = 5
-N = 3
-Farm = [[0,0,0,0,1],[0,0,0,0,0],[1,1,1,1,1]]
-solution(M,N,Farm)
+    print(count)
         
-        
+
+
+
 
 
     
